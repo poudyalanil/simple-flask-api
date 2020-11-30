46d054ef660c002e3273d939d1ad3ea435df2d89
poudyalanil <anilrunjakote@gmail.com>
2020-11-30 16:53:03 +0545

    Heroku Deplyoment

--- main.py
+++ main.py
@@ -34,7 +34,7 @@ def upload_file():
     if file and allowed_file(file.filename):
         filename = secure_filename(file.filename)
         file.save(os.path.join(app.config['UPLOAD_FOLDER'], filename))
-        resp = jsonify({'message': 'File successfully uploaded'})
+        resp = jsonify({'message': 'File successfully uplo aded'})
         resp.status_code = 201
         return resp
     else:
